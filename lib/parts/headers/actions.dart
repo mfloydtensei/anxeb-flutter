f184403206b074a2524251ea2888c4d1c1d7e5bf
Mateofloyd10 <163003528+Mateofloyd@users.noreply.github.com>
2025-10-31 01:19:29 -0400

    actions

--- lib/parts/headers/actions.dart
+++ lib/parts/headers/actions.dart
@@ -1,25 +1,29 @@
 import 'package:anxeb_flutter/middleware/header.dart';
-import 'package:anxeb_flutter/middleware/scope.dart';
+import 'package:anxeb_flutter/screen/scope.dart';
 import 'package:anxeb_flutter/misc/action_icon.dart';
 import 'package:flutter/material.dart';
 
 class ActionsHeader extends ScreenHeader {
-  Widget Function() _body;
+  final Widget Function()? _body;
+  final List<ActionItem>? actions;
+  final Widget? leadingWidget; // ✅ Se define localmente el "leading"
 
   ActionsHeader({
-    Scope scope,
-    Widget Function() title,
-    Widget Function() body,
-    Widget Function() bottom,
-    double Function() elevation,
-    double Function() height,
+    required ScreenScope scope,
+    Widget Function()? title,
+    Widget Function()? body,
+    Widget Function()? bottom,
+    double Function()? elevation,
+    double Function()? height,
     this.actions,
-    VoidCallback dismiss,
-    VoidCallback back,
-    ActionIcon leading,
-    bool Function() isVisible,
-    Color Function() fill,
-  }) : super(
+    VoidCallback? dismiss,
+    VoidCallback? back,
+    ActionIcon? leading,
+    bool Function()? isVisible,
+    Color Function()? fill,
+  })  : _body = body,
+        leadingWidget = leading?.build(), // ✅ Guardamos el widget aquí
+        super(
           scope: scope,
           dismiss: dismiss,
           back: back,
@@ -29,30 +33,32 @@ class ActionsHeader extends ScreenHeader {
           height: height,
           isVisible: isVisible,
           fill: fill,
-        ) {
-    super.leading = leading?.build();
-    _body = body;
-  }
-
-  @protected
-  List<ActionItem> actions;
+        );
 
   @override
-  Widget body() {
-    return _body?.call() ?? null;
-  }
+  Widget? body() => _body?.call();
 
   @override
   List<Widget> content() {
-    var $actions = actions != null ? actions.where(($action) => $action.isVisible?.call() != false).map(($action) => $action.build()).toList() : null;
-    return $actions;
+    final visibleActions = actions
+            ?.where((a) => a.isVisible?.call() ?? true)
+            .map((a) => a.build())
+            .toList() ??
+        [];
+
+    // ✅ Si existe un leading, lo agregamos como primer elemento
+    if (leadingWidget != null) {
+      return [leadingWidget!, ...visibleActions];
+    }
+
+    return visibleActions;
   }
 }
 
 abstract class ActionItem {
-  bool Function() isDisabled;
-  bool Function() isVisible;
-  VoidCallback onPressed;
+  bool Function()? isDisabled;
+  bool Function()? isVisible;
+  VoidCallback? onPressed;
 
   Widget build();
 }
